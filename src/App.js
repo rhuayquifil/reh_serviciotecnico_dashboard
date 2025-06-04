a3107453219b37e74c719a147dfcc1d162679ea0
rhuayquifil <ezehuayquifil@hotmail.com>
2025-06-04 19:47:06 -0300

    process view home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Cards } from './components/Cards';
+import { CarouselSlide } from './components/CarouselSlide';
+import  { Header }  from './components/Header';
+import { QuestionsFrequently } from './components/QuestionsFrequently ';
+import { Ubication } from './components/Ubication';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <CarouselSlide />
+      <Cards />
+      <Ubication />
+      <QuestionsFrequently />
+      <h4>Nosotros</h4>
+      <h4>Terminos y condiciones</h4>
     </div>
   );
 }
